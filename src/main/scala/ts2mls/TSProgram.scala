58cf1f6490a576b756f8ab43e059180fc2956a16
NeilKleistGao <neilkleist@gmail.com>
2022-07-19 17:55:33 +0800

    Add CI

--- src/main/scala/ts2mls/TSProgram.scala
+++ src/main/scala/ts2mls/TSProgram.scala
@@ -9,6 +9,10 @@ class TSProgram(filename: String) {
   private val program: js.Dynamic = ts.createProgram(js.Array(filename), js.Dictionary("maxNodeModuleJsDepth" -> 0))
   private val checker: js.Dynamic = program.getTypeChecker()
   private val sourceFile: js.Dynamic = program.getSourceFile(filename)
+
+  type ASTNode = ts.Node
+
+  def visit(func: (ASTNode => Unit)): Unit = ts.forEachChild(sourceFile, func)
 }
 
 object TSProgram {
