ecae54fe6dc2dcc8b463e408caa737d898e76e39
NeilKleistGao <neilkleist@gmail.com>
2022-07-20 18:01:52 +0800

    Remove redundent ', '

--- src/main/scala/ts2mls/types/TSType.scala
+++ src/main/scala/ts2mls/types/TSType.scala
@@ -11,6 +11,8 @@ case class TSPrimitiveType(typeName: String) extends TSType {
 case class TSFunctionType(params: List[TSType], res: TSType) extends TSType {
   override def toString(): String =
     if (params.length == 0) res.toString()
-    else s"(${params.foldLeft("")((ls: String, p: TSType) => ls + ", " + p.toString())}) => ${res.toString()}"
-    
+    else {
+      val ps = params.foldLeft("")((ls: String, p: TSType) => ls + p.toString() + ", ")
+      s"(${ps.substring(0, ps.length() - 2)}) => ${res.toString()}"
+    }
 }
