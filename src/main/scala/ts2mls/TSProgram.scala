6e373143cf7e0c4c6bb4b8686bbe8615032ff417
NeilKleistGao <neilkleist@gmail.com>
2022-07-27 16:16:17 +0800

    Use js.isUndefined instead

--- src/main/scala/ts2mls/TSProgram.scala
+++ src/main/scala/ts2mls/TSProgram.scala
@@ -36,7 +36,7 @@ class TSProgram(filename: String) {
         val typeInfo = parseInterfaceMembers(node)
         types += iName -> typeInfo
       }
-      else if (node.symbol.exports != js.undefined) {
+      else if (!js.isUndefined(node.symbol.exports)) {
         val nsName = nodeObject.symbol.escapedName
         val typeInfo = parseNamespace(node)
         types += nsName -> typeInfo
@@ -53,23 +53,23 @@ class TSProgram(filename: String) {
 
   private def getTypeConstraints(list: js.Dynamic, prev: Map[String, TSType]): Map[String, TSType] = {
     val tail = list.pop()
-    if (tail == js.undefined) prev
-    else if (tail.constraint == js.undefined) getTypeConstraints(list, prev)
+    if (js.isUndefined(tail)) prev
+    else if (js.isUndefined(tail.constraint)) getTypeConstraints(list, prev)
     else getTypeConstraints(list, prev) ++ Map(tail.symbol.escapedName.toString() -> getElementType(tail.constraint))
   }
 
   private def getTypeConstraints(node: ts.Node): Map[String, TSType] = {
-    if (node.typeParameters == js.undefined) Map()
+    if (js.isUndefined(node.typeParameters)) Map()
     else getTypeConstraints(node.typeParameters, Map())
   } 
 
   private def getFunctionType(node: ts.Node): TSFunctionType = {
     val params = node.parameters
-    val pList = if (params == js.undefined) List() else getFunctionParametersType(params)
+    val pList = if (js.isUndefined(params)) List() else getFunctionParametersType(params)
     val signature = checker.getSignatureFromDeclaration(node)
     val res = checker.getReturnTypeOfSignature(signature)
-    if (res.symbol != js.undefined && res.symbol.declarations.length > 0) {
-      if (res.resolvedTypeArguments != js.undefined)
+    if (!js.isUndefined(res.symbol) && res.symbol.declarations.length > 0) {
+      if (!js.isUndefined(res.resolvedTypeArguments))
         new TSFunctionType(pList, new TSArrayType(getElementType(res.resolvedTypeArguments.shift())), getTypeConstraints(node))
       else
         new TSFunctionType(pList, getFunctionType(res.symbol.declarations.shift()), getTypeConstraints(node))
@@ -80,15 +80,15 @@ class TSProgram(filename: String) {
 
   private def getElementType(token: js.Dynamic): TSType = {
     val tp = token.selectDynamic("type")
-    if (tp != js.undefined) getFunctionType(tp.symbol.declarations.shift())
+    if (!js.isUndefined(tp)) getFunctionType(tp.symbol.declarations.shift())
     else
-      if (token.intrinsicName != js.undefined) new TSNamedType(token.intrinsicName.toString) 
+      if (!js.isUndefined(token.intrinsicName)) new TSNamedType(token.intrinsicName.toString) 
       else new TSNamedType(checker.getTypeFromTypeNode(token).intrinsicName.toString)
   }
 
   private def getUnionType(types: js.Dynamic, prev: TSUnionType): TSUnionType = {
     val t = types.pop()
-    if (t == js.undefined) prev
+    if (js.isUndefined(t)) prev
     else getUnionType(types, new TSUnionType(getElementType(t), prev))
   }
 
@@ -101,20 +101,20 @@ class TSProgram(filename: String) {
 
   private def getFunctionParameterType(node: ts.node): TSType = {
     val typeNode = node.selectDynamic("type")
-    if (typeNode != js.undefined && ts.isFunctionTypeNode(typeNode)) getFunctionType(typeNode)
-    else if (typeNode != js.undefined && ts.isArrayTypeNode(typeNode)) new TSArrayType(getElementType(typeNode.elementType))
-    else if (typeNode != js.undefined && typeNode.types != js.undefined && typeNode.types.length > 1) getUnionType(typeNode.types)
+    if (!js.isUndefined(typeNode) && ts.isFunctionTypeNode(typeNode)) getFunctionType(typeNode)
+    else if (!js.isUndefined(typeNode) && ts.isArrayTypeNode(typeNode)) new TSArrayType(getElementType(typeNode.elementType))
+    else if (!js.isUndefined(typeNode) && !js.isUndefined(typeNode.types) && typeNode.types.length > 1) getUnionType(typeNode.types)
     else getNamedType(TSSymbolObject(node.symbol))
   }
 
   private def getFunctionParametersType(list: js.Dynamic): List[TSType] = {
     val tail = list.pop()
-    if (tail == js.undefined) List() else getFunctionParametersType(list) :+ getFunctionParameterType(tail)
+    if (js.isUndefined(tail)) List() else getFunctionParametersType(list) :+ getFunctionParameterType(tail)
   }
 
   private def getClassMembersType(list: js.Dynamic): Map[String, TSType] = {
     val tail = list.pop()
-    if (tail == js.undefined) Map()
+    if (js.isUndefined(tail)) Map()
     else {
       val name = tail.symbol.escapedName.toString
       if (ts.isMethodDeclaration(tail) && !name.equals("__constructor")) // TODO: we assumed that there is no inner class
@@ -125,11 +125,11 @@ class TSProgram(filename: String) {
 
   private def getInterfacePropertiesType(list: js.Dynamic): Map[String, TSType] = {
     val tail = list.pop()
-    if (tail == js.undefined) Map()
+    if (js.isUndefined(tail)) Map()
     else {
       val name = tail.symbol.escapedName.toString
       val typeObject = tail.symbol.valueDeclaration.selectDynamic("type")
-      if (typeObject.parameters != js.undefined)
+      if (!js.isUndefined(typeObject.parameters))
         getInterfacePropertiesType(list) ++ Map(name -> getFunctionType(typeObject))
       else getInterfacePropertiesType(list) ++ Map(name -> getNamedType(TSSymbolObject(tail.symbol)))
     }
@@ -160,7 +160,7 @@ class TSProgram(filename: String) {
       if (ts.isFunctionDeclaration(node)) parseNamespaceExports(it) ++ Map(name -> getFunctionType(node))
       else if (ts.isClassDeclaration(node)) parseNamespaceExports(it) ++ Map(name -> parseClassMembers(node))
       else if (ts.isInterfaceDeclaration(node)) parseNamespaceExports(it) ++ Map(name -> parseInterfaceMembers(node))
-      else if (node.symbol.exports != js.undefined) parseNamespaceExports(it) ++ Map(name -> parseNamespace(node))
+      else if (!js.isUndefined(node.symbol.exports)) parseNamespaceExports(it) ++ Map(name -> parseNamespace(node))
       else parseNamespaceExports(it)
     }
   }
