58f6fc94a14fd04203b7aec0ee7e040ee0e7159c
NeilKleistGao <neilkleist@gmail.com>
2022-07-27 15:53:11 +0800

    Add ts object

--- src/main/scala/ts2mls/TSCompilerInfo.scala
+++ src/main/scala/ts2mls/TSCompilerInfo.scala
@@ -4,28 +4,41 @@ import scala.scalajs.js
 import js.Dynamic.{global => g}
 import js.DynamicImplicits._
 
-class TSSymbolObject(node: js.Dynamic, ts: js.Dynamic) {
-    lazy val escapedName: String = node.escapedName.toString
+object TypeScript {
+  private lazy val ts = g.require("typescript")
 
+  lazy val ModifierFlagsExport: Int = ts.ModifierFlags.Export.asInstanceOf[Int]
+  lazy val SyntaxKindSourceFile: Int = ts.SyntaxKind.SourceFile.asInstanceOf[Int]
+
+  def isToken(node: js.Dynamic): Boolean = ts.isToken(node)
+  def isFunctionDeclaration(node: js.Dynamic): Boolean = ts.isFunctionDeclaration(node)
+  def isClassDeclaration(node: js.Dynamic): Boolean = ts.isClassDeclaration(node)
+  def isInterfaceDeclaration(node: js.Dynamic): Boolean = ts.isInterfaceDeclaration(node)
+  def getCombinedModifierFlags(node: js.Dynamic): Int = ts.getCombinedModifierFlags(node).asInstanceOf[Int]
+}
+
+class TSSymbolObject(node: js.Dynamic) {
+  lazy val escapedName: String = node.escapedName.toString
 }
 
 object TSSymbolObject {
-    def apply(node: js.Dynamic)(implicit ts: js.Dynamic) = new TSSymbolObject(node, ts)
+  def apply(node: js.Dynamic) = new TSSymbolObject(node)
 }
 
-class TSNodeObject(node: js.Dynamic, ts: js.Dynamic) {
-    lazy val isToken: Boolean = ts.isToken(node)
-    lazy val isFunctionDeclaration: Boolean = ts.isFunctionDeclaration(node)
-    lazy val isClassDeclaration: Boolean = ts.isClassDeclaration(node)
-    lazy val isInterfaceDeclaration: Boolean = ts.isInterfaceDeclaration(node)
-    lazy val hasExportModifier: Boolean = (ts.getCombinedModifierFlags(node) & ts.ModifierFlags.Export) != 0
-    lazy val isNull: Boolean = node == null
-    lazy val isSourceFile: Boolean = node.kind == ts.SyntaxKind.SourceFile
+class TSNodeObject(node: js.Dynamic) {
+  lazy val isToken: Boolean = TypeScript.isToken(node)
+  lazy val isFunctionDeclaration: Boolean = TypeScript.isFunctionDeclaration(node)
+  lazy val isClassDeclaration: Boolean = TypeScript.isClassDeclaration(node)
+  lazy val isInterfaceDeclaration: Boolean = TypeScript.isInterfaceDeclaration(node)
+  lazy val hasExportModifier: Boolean = (TypeScript.getCombinedModifierFlags(node) & TypeScript.ModifierFlagsExport) != 0
+  lazy val isNull: Boolean = node == null
+  lazy val kind: Int = node.kind.asInstanceOf[Int]
+  lazy val isSourceFile: Boolean = kind == TypeScript.SyntaxKindSourceFile
 
-    lazy val parent: TSNodeObject = if (!isNull) TSNodeObject(node.parent)(ts) else throw new java.lang.Exception("Access parent of null node.")
-    lazy val symbol: TSSymbolObject = TSSymbolObject(node.symbol)(ts)
+  lazy val parent: TSNodeObject = if (!isNull) TSNodeObject(node.parent) else throw new java.lang.Exception("Access parent of null node.")
+  lazy val symbol: TSSymbolObject = TSSymbolObject(node.symbol)
 }
 
 object TSNodeObject {
-    def apply(node: js.Dynamic)(implicit ts: js.Dynamic) = new TSNodeObject(node, ts)
+  def apply(node: js.Dynamic) = new TSNodeObject(node)
 }
--- src/main/scala/ts2mls/TSProgram.scala
+++ src/main/scala/ts2mls/TSProgram.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable.HashMap
 import ts2mls.types._
 
 class TSProgram(filename: String) {
-  implicit private val ts: js.Dynamic = g.require("typescript")
+  private val ts: js.Dynamic = g.require("typescript")
   private val program: js.Dynamic = ts.createProgram(js.Array(filename), js.Dictionary("maxNodeModuleJsDepth" -> 0))
   private val checker: js.Dynamic = program.getTypeChecker()
   private val sourceFile: js.Dynamic = program.getSourceFile(filename)
