eaef062abed94bb6fbd1236a2c35b81120220062
NeilKleistGao <neilkleist@gmail.com>
2022-07-29 10:06:42 +0800

    Add basic convert test

--- src/main/scala/ts2mls/types/Converter.scala
+++ src/main/scala/ts2mls/types/Converter.scala
@@ -3,6 +3,13 @@ package ts2mls.types
 import mlscript._
 
 object Converter {
-  // TODO:
-  def convert(tsType: TSType): Type = Top
+  def convert(tsType: TSType): Type = tsType match {
+    case TSNamedType(typeName) => new TypeName(typeName)
+    case TSFunctionType(params, res, constraint) => 
+      new Function(params.foldLeft[Type](Bot)((mlst, tst) => mlst match {
+        case Bot => convert(tst)
+        case _ => new Function(mlst, convert(tst))
+      }), convert(res))
+    case _ => new TypeName("") // TODO:
+  }
 }
--- src/test/scala/BasicFunction.scala
+++ src/test/scala/BasicFunction.scala
@@ -1,6 +1,7 @@
 import org.scalatest.funsuite.AnyFunSuite
 import ts2mls.TSProgram
 import ts2mls.types._
+import mlscript._
 
 class BasicFunction extends AnyFunSuite {
   test("Basic Function") {
@@ -14,4 +15,16 @@ class BasicFunction extends AnyFunSuite {
     assert(TypeCompare(program.getType("isnull"), "any => boolean"))
     assert(TypeCompare(program.getType("bar"), "any"))
   }
+
+  test("Basic Convert") {
+    val program = TSProgram("src/test/typescript/BasicFunctions.ts")
+
+    program.getMLSType("hello") match {
+      case Function(lhs, rhs) => rhs match {
+        case TypeName(name) if (name.equals("void")) => assert(true)
+        case _ => assert(false)
+      }
+      case _ => assert(false)
+    }
+  } 
 }
