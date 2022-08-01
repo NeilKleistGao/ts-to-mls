768282d9cc0243438e0ccb6a350ffd7cb601dc89
NeilKleistGao <neilkleist@gmail.com>
2022-08-01 12:24:20 +0800

    Add inter convert

--- src/main/scala/ts2mls/types/Converter.scala
+++ src/main/scala/ts2mls/types/Converter.scala
@@ -4,23 +4,28 @@ import mlscript._
 
 object Converter {
   private val primitiveName = Map[String, Type](
-    "boolean" -> new TypeName("bool"),
-    "number" -> new TypeName("number"),
-    "string" -> new TypeName("string"),
+    "boolean" -> TypeName("bool"),
+    "number" -> TypeName("number"),
+    "string" -> TypeName("string"),
     "any" -> Top,
-    "void" -> new TypeName("unit"),
-    "null" -> new TypeName("null"),
-    "undefined" -> new TypeName("undefined"),
+    "void" -> TypeName("unit"),
+    "null" -> TypeName("null"),
+    "undefined" -> TypeName("undefined"),
     "never" -> Bot,
-    "object" -> new Record(List())
+    "object" -> Record(List())
   )
 
   def convert(tsType: TSType): Type = tsType match {
     case TSNamedType(typeName) => primitiveName(typeName)
     case TSFunctionType(params, res, constraint) =>
-      if (params.length == 0) new Function(primitiveName("void"), convert(res))
-      else params.foldRight[Type](convert(res))((tst, mlst) => new Function(convert(tst), mlst))
-    case TSUnionType(lhs, rhs) => new Union(convert(lhs), convert(rhs))
-    case _ => new TypeName("") // TODO:
+      if (params.length == 0) Function(primitiveName("void"), convert(res))
+      else params.foldRight[Type](convert(res))((tst, mlst) => Function(convert(tst), mlst))
+    case TSUnionType(lhs, rhs) => Union(convert(lhs), convert(rhs))
+    case TSIntersectionType(lhs, rhs) => Inter(convert(lhs), convert(rhs))
+    case v: TSTypeVariable => convertTypeVariable(v)
+    case _ => new TypeName("") // TODO: more types support
   }
+
+  // TODO: only for intersection test
+  private def convertTypeVariable(tstv: TSTypeVariable): Type = TypeName(tstv.toString())
 }
--- src/test/scala/Intersection.scala
+++ src/test/scala/Intersection.scala
@@ -5,9 +5,31 @@ import ts2mls.types._
 class Intersection extends AnyFunSuite {
   test("Intersection") {
     val program = TSProgram(Seq("src/test/typescript/Intersection.ts"))
-    System.out.println(s"rua: ${program.>("extend")}")
     assert(TypeCompare(program.>("extend"), "(T, U) => (T & U)"))
     assert(TypeCompare(program.>("foo"), "(T & U) => void"))
+  }
+
+  test("Intersection Convert") {
+    import mlscript._
+
+    val program = TSProgram(Seq("src/test/typescript/Intersection.ts"))
+    program.getMLSType("foo") match {
+      case Function(p, r) => p match {
+        case Inter(lhs, rhs) => {
+          // TODO: use named types rather than type variables temporarily
+          lhs match {
+            case TypeName(name) => assert(name.equals("T"))
+            case _ => assert(false)
+          }
+
+          rhs match {
+            case TypeName(name) => assert(name.equals("U"))
+            case _ => assert(false)
+          }
+        }
+      }
+      case _ => assert(false)
+    }
   }
 }
 
