ee02bd72c758c059a6bdba900a3c2f91faa1e9d2
NeilKleistGao <neilkleist@gmail.com>
2022-08-02 17:03:36 +0800

    Add tuple convert test

--- src/test/scala/Tuple.scala
+++ src/test/scala/Tuple.scala
@@ -10,4 +10,38 @@ class Tuple extends AnyFunSuite {
     assert(TypeCompare(program.>("third"), "([number, number, number]) => number"))
     assert(TypeCompare(program.>("vec2"), "(number, number) => [number, number]"))
   }
+
+  test("Tuple Convert") {
+    import mlscript._
+
+    val program = TSProgram(Seq("src/test/typescript/Tuple.ts"))
+
+    program.getMLSType("key") match {
+      case Function(p, r) => p match {
+        case mlscript.Tuple(lst) => {
+          lst(0) match {
+            case None -> Field(in, out) => {
+              assert(in.isEmpty)
+              out match {
+                case TypeName(name) => assert(name.equals("string"))
+                case _ => assert(false)
+              }
+            }
+          }
+
+          lst(1) match {
+            case None -> Field(in, out) => {
+              assert(in.isEmpty)
+              out match {
+                case TypeName(name) => assert(name.equals("bool"))
+                case _ => assert(false)
+              }
+            }
+          }
+        }
+        case _ => assert(false)
+      } 
+      case _ => assert(false)
+    }
+  }
 }
