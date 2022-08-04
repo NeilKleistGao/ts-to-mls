9ee6511fd4317936e204bfe5f364da816949b092
NeilKleistGao <neilkleist@gmail.com>
2022-08-04 10:18:12 +0800

    Add enum convert

--- src/main/scala/ts2mls/types/Converter.scala
+++ src/main/scala/ts2mls/types/Converter.scala
@@ -33,7 +33,8 @@ object Converter {
     case TSIntersectionType(lhs, rhs) => Inter(convert(lhs), convert(rhs))
     case v: TSTypeVariable => convertTypeVariable(v)
     case TSTupleType(lst) => convertTuple(lst)
-    case TSArrayType(elementType) => TypeName("MutArray")
+    case TSArrayType(_) => TypeName("MutArray")
+    case TSEnumType(_) => TypeName("int")
     case _ => throw new java.lang.Exception("Unknown TypeScript Type") // TODO: more types support
   }
 
--- src/test/scala/Enum.scala
+++ src/test/scala/Enum.scala
@@ -8,4 +8,18 @@ class Enum extends AnyFunSuite {
     assert(TypeCompare(program.>("pass"), "(Color) => boolean"))
     assert(TypeCompare(program.>("stop"), "Color"))
   }
+
+  test("Enum Convert") {
+    import mlscript._
+
+    val program = TSProgram(Seq("src/test/typescript/Enum.ts"))
+
+    program.getMLSType("stop") match {
+      case Function(p, r) => r match {
+        case TypeName(name) => assert(name.equals("int"))
+        case _ => assert(false)
+      }
+      case _ => assert(false)
+    }
+  }
 }
