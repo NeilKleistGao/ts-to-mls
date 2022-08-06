adf0b40187df06bcbadb5031102f3312fc117792
NeilKleistGao <neilkleist@gmail.com>
2022-08-06 10:25:07 +0800

    Add optional tests

--- src/test/scala/Optional.scala
+++ src/test/scala/Optional.scala
@@ -11,5 +11,14 @@ class Optional extends AnyFunSuite {
 
     assert(TypeCompare(program.>("SquareConfig").>("color"), "string | undefined"))
     assert(TypeCompare(program.>("SquareConfig").>("width"), "number | undefined"))
+
+    assert(TypeCompare(program.>("did"), "(number, (number) => number | undefined) => number"))
+    assert(TypeCompare(program.>("getOrElse"), "(object[] | undefined) => object"))
+    assert(TypeCompare(program.>("testABC"), "(ABC | undefined) => void"))
+    assert(TypeCompare(program.>("testSquareConfig"), "(SquareConfig | undefined) => void"))
+    assert(TypeCompare(program.>("err"), "([number, string] | undefined) => void"))
+    assert(TypeCompare(program.>("toStr"), "(number | boolean | undefined) => string"))
+    assert(TypeCompare(program.>("boo"), "(T' & U' | undefined) => void"))
+    assert(TypeCompare(program.>("boom"), "(B<never> | undefined) => any"))
   }
 }
--- src/test/typescript/Optional.ts
+++ src/test/typescript/Optional.ts
@@ -15,3 +15,34 @@ interface SquareConfig {
   color?: string;
   width?: number;
 }
+
+function did(x: number, f?: (x: number) => number): number {
+  if (f === undefined) return x;
+  else return f(x);
+}
+
+function getOrElse(arr?: object[]): object {
+  if (arr === undefined) return {};
+  else return arr[0];
+}
+
+class ABC {}
+function testABC(abc?: ABC) {}
+
+function testSquareConfig(conf?: SquareConfig) {}
+
+function err(msg?: [number, string]): void {
+  if (msg === undefined) return;
+  else throw msg;
+}
+
+function toStr(x?: number | boolean): string {
+  if (x === undefined) return "undefined";
+  else return x.toString();
+}
+
+function boo<T, U>(x?: T & U) {}
+
+class B<T> {}
+
+function boom(b?: B<never>): any {}
