f786d195023c86763b325c1418a00547a00dd6be
NeilKleistGao <neilkleist@gmail.com>
2022-08-10 18:40:14 +0800

    Add namespace test

--- js/src/test/typescript/NameSpace.ts
+++ js/src/test/typescript/NameSpace.ts
@@ -24,3 +24,33 @@ namespace N1 {
     }
   }
 }
+
+namespace AA {
+  export function f(x) {
+    return "42";
+  }
+
+  export class C {
+    f() {}
+  }
+
+  export interface I {
+    f: () => number
+  }
+
+  export namespace N2 {}
+}
+
+/**
+  * @debug
+  */
+function f1(x: N1.C): N1.C {
+  return x;
+}
+
+/**
+  * @debug
+  */
+function f2(x: AA.C): AA.C {
+  return x;
+}
