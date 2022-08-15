64c49d28c459792fad3ff8fe49d1b7de373427c9
NeilKleistGao <neilkleist@gmail.com>
2022-08-15 11:14:11 +0800

    Add multiple files tests

--- js/src/test/scala/ts2mls/MultiFiles.scala
+++ js/src/test/scala/ts2mls/MultiFiles.scala
@@ -12,9 +12,20 @@ class MultiFiles extends AnyFunSuite {
     program.visit(writer)
     writer.close
   }
+
+  test("No File Generation") {
+    val program = TSProgram(MultiFiles.emptyTestFiles)
+    var writer = DecWriter(MultiFiles.emptyDiffFile)
+
+    program.visit(writer)
+    writer.close
+  }
 }
 
 object MultiFiles {
-  private val testFiles = TSTypeTest.tsPathes(Seq("Multi1.ts", "Multi2.ts"))
+  private val testFiles = TSTypeTest.tsPathes(Seq("Multi1.ts", "Multi2.ts", "Multi3.ts"))
   private val diffFile = TSTypeTest.diffPath("MultiFiles.d.mls")
+
+  private val emptyTestFiles = TSTypeTest.tsPathes(Seq())
+  private val emptyDiffFile = TSTypeTest.diffPath("Empty.d.mls")
 }
