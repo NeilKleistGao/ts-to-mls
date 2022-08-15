6e1b6c0ba986894cfd0c3328b8a66cb7cd37e236
NeilKleistGao <neilkleist@gmail.com>
2022-08-15 09:52:31 +0800

    Add union and inter tests

--- js/src/test/typescript/Intersection.ts
+++ js/src/test/typescript/Intersection.ts
@@ -9,3 +9,27 @@ function extend<T, U>(first: T, second: U): T & U {
 function foo<T, U>(x: T & U) {
   console.log(x)
 }
+
+function over(f: ((x: number) => string) & ((x: object) => string)): string {
+  return f(42);
+}
+
+interface IA{
+  x: number
+}
+
+interface IB{
+  y: number
+}
+
+function iii(x: IA & IB): IA & IB {
+  return x;
+}
+
+function uu<U, V, T, P>(x: (U | V) & (T | P)): (U | V) & (T | P) {
+  return x;
+}
+
+function iiii<U, T, V>(x: U & (T & V)): U & (T & V) {
+  return x;
+}
